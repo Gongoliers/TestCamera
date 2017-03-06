53cc3c5d1c05a9d4aea7de264dfb3b72a51746d8
Kyle Corry <kylecorry31@gmail.com>
2017-03-06 17:53:59 -0500

    Add ability to see hsv threshold and rectangle around target

--- src/org/usfirst/frc/team5112/robot/Robot.java
+++ src/org/usfirst/frc/team5112/robot/Robot.java
@@ -2,15 +2,20 @@ package org.usfirst.frc.team5112.robot;
 
 import java.util.List;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import com.kylecorry.frc.vision.CameraSource;
-import com.kylecorry.frc.vision.CameraSpecs;
 import com.kylecorry.frc.vision.TargetGroup;
 import com.kylecorry.frc.vision.TargetGroupDetector;
+import com.kylecorry.frc.vision.TargetSpecs;
+import com.thegongoliers.input.EnhancedXboxController;
 
+import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -28,8 +33,10 @@ public class Robot extends IterativeRobot {
 	final String customAuto = "My Auto";
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
+	SendableChooser<Integer> chooser2 = new SendableChooser<>();
 	TargetGroupDetector pegDetect;
 	CameraSource cam;
+	EnhancedXboxController xbox;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -37,16 +44,88 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
-		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-		camera.setResolution(160, 120);
-		camera.setBrightness(0);
-		camera.setExposureManual(0);
-		camera.setWhiteBalanceManual(10000);
-		cam = new CameraSource(camera);
-		pegDetect = new TargetGroupDetector(new PegRetroreflective(), new Peg());
+		chooser.addDefault("Show normal image", "normal");
+		chooser.addObject("Show HSV threshold", "threshold");
+		chooser.addObject("Outline target", "target");
+		SmartDashboard.putData("Camera Chooser", chooser);
+
+		chooser2.addDefault("Hue", 0);
+		chooser2.addObject("Saturation", 1);
+		chooser2.addObject("Value", 2);
+		SmartDashboard.putData("HSV Chooser", chooser2);
+
+		xbox = new EnhancedXboxController(0);
+
+		new Thread(() -> {
+
+			TargetSpecs specs = new PegRetroreflective();
+
+			double[][] hsv = { { specs.getHue().start, specs.getHue().end },
+					{ specs.getSaturation().start, specs.getSaturation().end },
+					{ specs.getValue().start, specs.getValue().end } };
+
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			camera.setResolution(160, 120);
+			camera.setBrightness(0);
+			camera.setExposureManual(0);
+			camera.setWhiteBalanceManual(10000);
+
+			cam = new CameraSource(camera);
+			pegDetect = new TargetGroupDetector(new PegRetroreflective(), new Peg());
+
+			Mat output = new Mat();
+			Mat source = new Mat();
+			CvSource outputStream = CameraServer.getInstance().putVideo("Camera", 160, 120);
+
+			while (!Thread.interrupted()) {
+
+				if (xbox.getPOV() != -1) {
+					int selected = chooser2.getSelected();
+					switch (xbox.getPOVDirection()) {
+					case NORTH:
+						hsv[selected][0] += 5;
+						System.out.println(hsv[selected][0]);
+						break;
+					case SOUTH:
+						hsv[selected][0] -= 5;
+						System.out.println(hsv[selected][0]);
+						break;
+					case EAST:
+						hsv[selected][1] += 5;
+						System.out.println(hsv[selected][1]);
+						break;
+					case WEST:
+						hsv[selected][1] -= 5;
+						System.out.println(hsv[selected][1]);
+						break;
+					default:
+						System.out.println("North and south control start; east and west control end");
+					}
+				}
+
+				source = cam.getPicture();
+				String choice = chooser.getSelected();
+				if (choice.equals("threshold")) {
+					Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2HSV);
+					Core.inRange(output, new Scalar(hsv[0][0], hsv[1][0], hsv[2][0]),
+							new Scalar(hsv[0][1], hsv[1][1], hsv[2][1]), output);
+				} else if (choice.equals("target")) {
+					List<TargetGroup> targets = pegDetect.detect(cam.getPicture());
+					if (!targets.isEmpty()) {
+						output = source;
+						Rect boundary = new Rect((int) Math.round(targets.get(0).getPosition().x),
+								(int) Math.round(targets.get(0).getPosition().y),
+								(int) Math.round(targets.get(0).getWidth()),
+								(int) Math.round(targets.get(0).getHeight()));
+						Imgproc.rectangle(output, boundary.tl(), boundary.br(), new Scalar(0, 255, 0));
+					}
+				} else {
+					output = source;
+				}
+				outputStream.putFrame(output);
+			}
+		}).start();
+
 	}
 
 	/**
@@ -62,7 +141,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		
+
 	}
 
 	/**
@@ -70,11 +149,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		List<TargetGroup> targets = pegDetect.detect(cam.getPicture());
-		if(!targets.isEmpty()){
-			TargetGroup best = targets.get(0);
-			System.out.println(best.computeAngle(160, CameraSpecs.MicrosoftLifeCam.HORIZONTAL_VIEW_ANGLE));
-		}
+
 	}
 
 	/**
@@ -91,4 +166,3 @@ public class Robot extends IterativeRobot {
 	public void testPeriodic() {
 	}
 }
-
